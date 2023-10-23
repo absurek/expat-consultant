90d7b69009688be86df35280c515a5b38765501f
absurek <surek.andras@gmail.com>
2023-10-23 20:58:40 +0200

    contact page improved

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,1 +1,61 @@
-<h2>Contact</h2>
+<script lang="ts">
+  import { IconMail, IconBrandInstagram, IconBrandLinkedin } from '@tabler/icons-svelte';
+</script>
+
+<svelte:head>
+  <title>Expat Consultant — Contact</title>
+</svelte:head>
+
+<section id="contact-info" class="mx-auto block max-w-screen-lg p-8 text-center">
+  <h1 class="h1">Contact Details</h1>
+  <div class="card mx-auto mt-8 max-w-xl p-4 text-left">
+    <div class="grid gap-1">
+      <ul>
+        <li>
+          <div class="-mx-2 flex items-center space-x-4 p-2">
+            <IconMail size={42} />
+            <div>
+              <p class="lg:text-xl">info@expat-consultant.com</p>
+              <a
+                class="text-tertiary-500 hover:text-tertiary-500/80 lg:text-xl"
+                href="mailto:info@expat-consultant.com"
+              >
+                E-mail
+              </a>
+            </div>
+          </div>
+        </li>
+        <li>
+          <div class="-mx-2 flex items-center space-x-4 p-2">
+            <IconBrandInstagram size={42} />
+            <div>
+              <p class="lg:text-xl">@expat_consultant_</p>
+              <a
+                class="text-tertiary-500 hover:text-tertiary-500/80 lg:text-xl"
+                href="https://www.instagram.com/expat_consultant_/"
+                target="_blank"
+              >
+                Instagram
+              </a>
+            </div>
+          </div>
+        </li>
+        <li>
+          <div class="-mx-2 flex items-center space-x-4 p-2">
+            <IconBrandLinkedin size={42} />
+            <div>
+              <p class="lg:text-xl">Falinora Kft.</p>
+              <a
+                class="text-tertiary-500 hover:text-tertiary-500/80 lg:text-xl"
+                href="https://www.linkedin.com/in/falinora-kft-245566260/"
+                target="_blank"
+              >
+                Linkedin
+              </a>
+            </div>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </div>
+</section>
