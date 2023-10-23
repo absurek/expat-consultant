2bfc51dbe0c3b0d3e09ade480074a475bcfee933
absurek <surek.andras@gmail.com>
2023-10-23 22:21:06 +0200

    page Hungary/Coworking Space improved

--- src/routes/hungary/coworking-space/+page.svelte
+++ src/routes/hungary/coworking-space/+page.svelte
@@ -1,1 +1,62 @@
-Coworking Space
+<script lang="ts">
+  import { IconMail, IconDeviceLandlinePhone, IconBrandInstagram } from '@tabler/icons-svelte';
+  import Details from './Details.svelte';
+</script>
+
+<svelte:head>
+  <title>Expat Consultant — Hungary | Coworking Space</title>
+</svelte:head>
+
+<section id="details" class="mx-auto max-w-screen-lg p-8">
+  <div class="prose prose-neutral mx-auto text-left dark:prose-invert lg:prose-xl">
+    <Details />
+  </div>
+</section>
+
+<section id="contact-info" class="mx-auto block max-w-screen-lg p-8 text-center">
+  <h1 class="h2">Contact</h1>
+  <div class="card mx-auto mt-8 max-w-xl p-4 text-left">
+    <div class="grid gap-1">
+      <ul>
+        <li>
+          <div class="-mx-2 flex items-center space-x-4 p-2">
+            <IconDeviceLandlinePhone size={42} />
+            <div>
+              <p class="lg:text-xl">+36 70 347 4618</p>
+              <a class="text-tertiary-500 hover:text-tertiary-500/80 lg:text-xl" href="tel:+36703474618"> Phone </a>
+            </div>
+          </div>
+        </li>
+        <li>
+          <div class="-mx-2 flex items-center space-x-4 p-2">
+            <IconMail size={42} />
+            <div>
+              <p class="lg:text-xl">info@expat-consultant.com</p>
+              <a
+                class="text-tertiary-500 hover:text-tertiary-500/80 lg:text-xl"
+                href="mailto:info@expat-consultant.com"
+              >
+                E-mail
+              </a>
+            </div>
+          </div>
+        </li>
+        <li>
+          <div class="-mx-2 flex items-center space-x-4 p-2">
+            <IconBrandInstagram size={42} />
+            <div>
+              <p class="lg:text-xl">@bajcsy_72</p>
+              <a
+                class="text-tertiary-500 hover:text-tertiary-500/80 lg:text-xl"
+                href="https://www.instagram.com/bajcsy_72/"
+                target="_blank"
+              >
+                Instagram
+              </a>
+            </div>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </div>
+</section>
---
+++ src/routes/hungary/coworking-space/Details.svelte
@@ -0,0 +1,62 @@
+<h1>Bajcsy 72</h1>
+<h2>Your Own Office in the Heart of Budapest</h2>
+<p>
+  Welcome to Bajcsy 72 Coworking Space! We are delighted to offer you a vibrant and collaborative work environment in
+  the heart of Budapest, catering to entrepreneurs and freelance professionals. Our coworking space provides a range of
+  services, including accountancy, photography, and legal consultation, to support your business needs. Below, you will
+  find our pricing options for different durations, whether you need a space for a few hours, a day, a month, or even a
+  whole year. We strive to provide flexible and affordable options to meet your requirements.
+</p>
+
+<h2>Price List*</h2>
+<ul>
+  <li>
+    <h3>Half a day Rates</h3>
+    <ul>
+      <li>Regular Desk: <strong>€12</strong></li>
+      <li>Dedicated Desk: <strong>€18</strong></li>
+    </ul>
+  </li>
+  <li>
+    <h3>Daily Rates</h3>
+    <ul>
+      <li>Regular Desk: <strong>€18</strong></li>
+      <li>Dedicated Desk: <strong>€24</strong></li>
+    </ul>
+  </li>
+  <li>
+    <h3>Monthly Rates</h3>
+    <h4>4 Days a Month</h4>
+    <ul>
+      <li>Regular Desk: <strong>€55</strong></li>
+      <li>Dedicated Desk: <strong>€76</strong></li>
+    </ul>
+    <h4>8 Days a Month</h4>
+    <ul>
+      <li>Regular Desk: <strong>€95</strong></li>
+      <li>Dedicated Desk: <strong>€122</strong></li>
+    </ul>
+    <h4>13 Days a Month</h4>
+    <ul>
+      <li>Regular Desk: <strong>€95</strong></li>
+      <li>Dedicated Desk: <strong>€122</strong></li>
+    </ul>
+    <h4>Monthly (Per Schedule)</h4>
+    <ul>
+      <li>Regular Desk: <strong>€150</strong></li>
+      <li>Dedicated Desk: <strong>€180</strong></li>
+    </ul>
+  </li>
+</ul>
+<p>
+  * Prices do not include VAT. <br />
+  * Please note that all prices are subject to change.
+</p>
+<p>
+  For a detailed breakdown of the services included with each option or any additional inquiries, please reach out to
+  our team using the contact information provided below.
+</p>
+<p>
+  At Bajcsy 72 we strive to create a dynamic community that fosters collaboration, networking, and growth. We look
+  forward to welcoming you to our coworking space and supporting you in your professional journey!
+</p>
