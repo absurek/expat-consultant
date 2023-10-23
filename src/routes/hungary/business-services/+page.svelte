86182b2ca4f806b58cfa5216c8c7109b0e7fe279
absurek <surek.andras@gmail.com>
2023-10-23 22:33:34 +0200

    page Hungary/Business Services improved

--- src/routes/hungary/business-services/+page.svelte
+++ src/routes/hungary/business-services/+page.svelte
@@ -1,1 +1,55 @@
-Business Services
+<script lang="ts">
+  import { IconMail, IconDeviceLandlinePhone } from '@tabler/icons-svelte';
+  import Details from './Details.svelte';
+</script>
+
+<svelte:head>
+  <title>Expat Consultant — Hungary | Business Services</title>
+</svelte:head>
+
+<section id="details" class="mx-auto max-w-screen-lg p-8">
+  <div class="prose prose-neutral mx-auto text-left dark:prose-invert lg:prose-xl">
+    <Details />
+  </div>
+</section>
+
+<section id="contact-info" class="mx-auto block max-w-screen-lg p-8 text-center">
+  <h2 class="h2">Contact</h2>
+  <div class="card mx-auto mt-8 max-w-xl p-4 text-left">
+    <div class="grid gap-1">
+      <ul>
+        <li>
+          <div class="-mx-2 flex items-center space-x-4 p-2">
+            <IconDeviceLandlinePhone size={42} />
+            <div>
+              <p class="lg:text-xl">+36 20 416 0038</p>
+              <a class="text-tertiary-500 hover:text-tertiary-500/80 lg:text-xl" href="tel:+36204160038">Phone</a>
+            </div>
+          </div>
+        </li>
+        <li>
+          <div class="-mx-2 flex items-center space-x-4 p-2">
+            <IconMail size={42} />
+            <div>
+              <p class="lg:text-xl">info@expat-consultant.com</p>
+              <a
+                class="text-tertiary-500 hover:text-tertiary-500/80 lg:text-xl"
+                href="mailto:info@expat-consultant.com"
+              >
+                E-mail
+              </a>
+            </div>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </div>
+
+  <div class="prose prose-neutral mx-auto py-8 text-left dark:prose-invert lg:prose-xl">
+    <p>
+      Please don't hesitate to reach out to us with any questions or to schedule a consultation. Our team is dedicated
+      to providing exceptional service and helping you achieve success in your financial endeavors. We look forward to
+      hearing from you!
+    </p>
+  </div>
+</section>
---
+++ src/routes/hungary/business-services/Details.svelte
@@ -0,0 +1,31 @@
+<h1>Must & Usefull Services for Businesses</h1>
+<p>
+  Whether you prefer Farsi, Arabic, English, or Hungarian, our team is here to assist you with your financial needs. We
+  offer basic, professional, and premium packages tailored to suit the requirements of students, entrepreneurs, and
+  business owners.
+</p>
+
+<h2>Accountancy</h2>
+<p>
+  Our experienced accountants ensure accurate bookkeeping, financial statements, and tax compliance, helping you
+  maintain a solid financial foundation for your business.
+</p>
+<p>We arrange private consultancies to teach you the basics and more about accountancy in Hungary.</p>
+
+<h2>Business Planning</h2>
+<p>
+  We provide expert guidance in developing business plans, analyzing market trends, and creating strategies to achieve
+  your goals.
+</p>
+
+<h2>Export and Import Consultation</h2>
+<p>
+  Our team offers valuable insights and advice for businesses engaged in international trade, helping you navigate the
+  complexities of export and import procedures.
+</p>
+
+<h2>Introduction to Business Setup in Hungary</h2>
+<p>
+  If you are interested in establishing your business in Hungary, we provide comprehensive support and guidance
+  throughout the setup process, including legal requirements and registration.
+</p>
