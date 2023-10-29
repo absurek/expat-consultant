eb217ea17e406b0acc1910c2959519f4ee631e5b
absurek <surek.andras@gmail.com>
2023-10-29 19:55:49 +0100

    page Portugal contents wrapped into accordion

--- src/routes/portugal/Business.svelte
+++ src/routes/portugal/Business.svelte
@@ -1,38 +1,55 @@
-<h3>D7 Residence Permit</h3>
-<p>
-  The D7 residence permit in Portugal is a type of permit designed for individuals who have sufficient funds to support
-  themselves while living in Portugal without the need for employment.
-</p>
-<p>
-  This permit is often sought after by retirees or individuals who have independent sources of income, such as remote
-  workers or entrepreneurs. To be eligible for a D7 residence permit, the applicant must demonstrate that they have
-  sufficient funds to support themselves and any dependents for the duration of their stay in Portugal. This can be
-  shown through proof of regular income, such as pensions, investments, or rental income. The minimum amount required
-  can vary, but it is generally around €7,200 per year for the applicant, plus an additional €3,600 per year for each
-  dependent.
-</p>
-<p>
-  Applicants for the D7 residence permit must also have health insurance that covers them while living in Portugal, and
-  they must pass a criminal background check.
-</p>
-<p>
-  The application process can be lengthy and requires the submission of various documents, including a criminal record
-  certificate, proof of financial means, and a certificate of accommodation.
-</p>
-<p>
-  Once approved, the D7 residence permit allows the holder to live and travel within Portugal for a period of one year,
-  renewable for successive periods of two years. After five years of continuous residency in Portugal, D7 permit holders
-  can apply for permanent residency or citizenship.
-</p>
+<script lang="ts">
+  import { Accordion, AccordionItem } from '@skeletonlabs/skeleton';
+</script>
 
-<h3>Different Types of Residence Permits</h3>
-<p>The residence permit categories for third-country nationals in Portugal are as follows:</p>
-<ul>
-  <li>Residence permit for work purposes</li>
-  <li>Residence permit for study purposes</li>
-  <li>Residence permit for family reunification purposes</li>
-  <li>Residence permit for investment purposes</li>
-  <li>Residence permit for retired persons or those who have sufficient means of subsistence</li>
-  <li>Residence permit for humanitarian reasons</li>
-  <li>Residence permit for the exercise of highly qualified activities</li>
-</ul>
+<Accordion>
+  <AccordionItem>
+    <svelte:fragment slot="summary">
+      <h3>D7 Residence Permit</h3>
+    </svelte:fragment>
+    <svelte:fragment slot="content">
+      <p>
+        The D7 residence permit in Portugal is a type of permit designed for individuals who have sufficient funds to
+        support themselves while living in Portugal without the need for employment.
+      </p>
+      <p>
+        This permit is often sought after by retirees or individuals who have independent sources of income, such as
+        remote workers or entrepreneurs. To be eligible for a D7 residence permit, the applicant must demonstrate that
+        they have sufficient funds to support themselves and any dependents for the duration of their stay in Portugal.
+        This can be shown through proof of regular income, such as pensions, investments, or rental income. The minimum
+        amount required can vary, but it is generally around €7,200 per year for the applicant, plus an additional
+        €3,600 per year for each dependent.
+      </p>
+      <p>
+        Applicants for the D7 residence permit must also have health insurance that covers them while living in
+        Portugal, and they must pass a criminal background check.
+      </p>
+      <p>
+        The application process can be lengthy and requires the submission of various documents, including a criminal
+        record certificate, proof of financial means, and a certificate of accommodation.
+      </p>
+      <p>
+        Once approved, the D7 residence permit allows the holder to live and travel within Portugal for a period of one
+        year, renewable for successive periods of two years. After five years of continuous residency in Portugal, D7
+        permit holders can apply for permanent residency or citizenship.
+      </p>
+    </svelte:fragment>
+  </AccordionItem>
+  <AccordionItem>
+    <svelte:fragment slot="summary">
+      <h3>Different Types of Residence Permits</h3>
+    </svelte:fragment>
+    <svelte:fragment slot="content">
+      <p>The residence permit categories for third-country nationals in Portugal are as follows:</p>
+      <ul>
+        <li>Residence permit for work purposes</li>
+        <li>Residence permit for study purposes</li>
+        <li>Residence permit for family reunification purposes</li>
+        <li>Residence permit for investment purposes</li>
+        <li>Residence permit for retired persons or those who have sufficient means of subsistence</li>
+        <li>Residence permit for humanitarian reasons</li>
+        <li>Residence permit for the exercise of highly qualified activities</li>
+      </ul>
+    </svelte:fragment>
+  </AccordionItem>
+</Accordion>
